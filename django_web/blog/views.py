de1da480a5e99ca1a0f7255771cc31708c71edf7
hamzatan.ht@outlook.com <hamzatan.ht@outlook.com>
2019-10-15 21:51:31 -0400

    Updated Database definitions for Blog, created table for Posts that a user can create. Added Posts model to Admin.py for managing through admin website. Created new app in Django for user registrationa and registered with apps.py

--- django_web/blog/views.py
+++ django_web/blog/views.py
@@ -1,18 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author':'Hamza Tanveer',
-        'title':'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2019'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
